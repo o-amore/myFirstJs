8c0561b455d5cca6888881db5482dece0522cdf6
o-amore <favouramore2211@gmail.com>
2021-07-02 12:08:54 +0100

    r Initial commit

---
+++ demo.js
@@ -0,0 +1,29 @@
+console.log("hello  world");
+
+// let a;
+// a = 5;
+// console.log(a);
+
+//  let A = 6;
+// console.log(A);
+
+let e = 7;
+let b = 7;
+const sum = e === b;
+console.log(sum);
+
+const name = "John Wick";
+const greet = "Morning";
+const result = `This is Seun and  ${name}`;
+
+console.log(result);
+
+//objects
+const estate = {
+    name: "Ojuelegba", 
+    noOfBlocks:  20,
+
+};
+
+// typeof(estate);
+console.log(estate);
